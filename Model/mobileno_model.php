3e88fd60d7bbe2f4f6ab0dcda896978b56ec78ba
HiddenIsSquid <amirhaini@gmail.com>
2021-10-26 09:43:55 +0800

    add

--- Model/mobileno_model.php
+++ Model/mobileno_model.php
@@ -29,7 +29,7 @@ function postAdmin() {
     $sql = "DELETE FROM user WHERE USER_ID = $last_id;";
     //execute
     $conn->query($sql);
-    header('Location: http://localhost/backend/try/View/adminCheck.php');
+    header('Location: http://68.183.177.183/mysejahteri/View/adminCheck.php');
 }
 
 
@@ -43,7 +43,7 @@ function postReturn(){
     $sql = "DELETE FROM user WHERE USER_ID = $last_id;";
     //execute
     $conn->query($sql);
-    header('Location: http://localhost/backend/try/index.php');
+    header('Location: http://68.183.177.183/mysejahteri/try/index.php');
 }
 
 
